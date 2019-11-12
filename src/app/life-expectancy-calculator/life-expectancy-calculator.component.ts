6a6291331e979fb87a1982b7de56dd9e71364f0d
Tomáš Rehák <trehak@ebridge.sk>
2019-11-12 20:48:45 +0100

    feat: style overview, calculator, and create calculator form

--- src/app/life-expectancy-calculator/life-expectancy-calculator.component.ts
+++ src/app/life-expectancy-calculator/life-expectancy-calculator.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-life-expectancy-calculator',
-  templateUrl: './life-expectancy-calculator.component.html',
-  styleUrls: ['./life-expectancy-calculator.component.css']
+    selector: 'app-life-expectancy-calculator',
+    templateUrl: './life-expectancy-calculator.component.html',
+    styleUrls: ['./life-expectancy-calculator.component.css']
 })
 export class LifeExpectancyCalculatorComponent implements OnInit {
+    public expectancyForm;
 
-  constructor() { }
+    constructor(private formBuilder: FormBuilder) {
+        this.expectancyForm = this.formBuilder.group({
+            sex: '',
+            country: '',
+            date: '',
+            age: ''
+        });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    onSubmit(customerData) {
+        console.log(customerData);
+        // Process checkout data here
+        // console.warn('Your order has been submitted', customerData);
+
+        // this.items = this.cartService.clearCart();
+        // this.checkoutForm.reset();
+    }
 
 }
